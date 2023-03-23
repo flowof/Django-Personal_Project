ac8666d97ac6c7f79323a040a24e2bd90ec56561
Minjoo Park <01140114mjp@donga.ac.kr>
2023-03-24 05:42:57 +0900

    230324

---
+++ Inflearn/Inflearn_lecture/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import myText
+
+# Create your views here.
+def home_list(request):
+
+    texts = myText.objects.filter() # myText에 있는 모든 정보 가져와서 texts라는 변수에 저장
+    return render(request, 'Inflearn_lecture\home_list.html', {'texts': texts}) # texts라는 정보를 html과 함께 보여줌
